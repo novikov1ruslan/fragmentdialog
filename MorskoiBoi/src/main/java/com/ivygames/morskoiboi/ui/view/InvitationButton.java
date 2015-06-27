1f608260bfdf601813c834afc983bbe0b238e918
Novikov, Ruslan <ruslan.novikov@here.com>
2015-06-27 23:22:06 +0200

    FIX: java.lang.NullPointerException 	at com.ivygames.morskoiboi.ui.view.InvitationButton.onSizeChanged(InvitationButton.java:57)

--- MorskoiBoi/src/main/java/com/ivygames/morskoiboi/ui/view/InvitationButton.java
+++ MorskoiBoi/src/main/java/com/ivygames/morskoiboi/ui/view/InvitationButton.java
@@ -11,50 +11,60 @@ import android.widget.Button;
 
 import com.ivygames.morskoiboi.R;
 
+import org.commons.logger.Ln;
+
 public class InvitationButton extends Button {
 
-	private final Bitmap mBitmap;
-	private int mLeft;
-	private int mTop;
-	private Paint mPaint;
-	private boolean mShowInvitation;
+    private final Bitmap mInvitation;
+    private int mInvitationLeft;
+    private int mInvitationTop;
+    private Paint mPaint;
+    private boolean mShowInvitation;
 
-	public InvitationButton(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		mBitmap = createInvitationBitmap();
-	}
+    public InvitationButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mInvitation = createInvitationBitmap();
+    }
 
-	public InvitationButton(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		mBitmap = createInvitationBitmap();
-	}
+    public InvitationButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        mInvitation = createInvitationBitmap();
+    }
 
-	private Bitmap createInvitationBitmap() {
-		return BitmapFactory.decodeResource(getResources(), R.drawable.invitation);
-	}
+    private Bitmap createInvitationBitmap() {
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.invitation);
+        if (bitmap == null) {
+            Ln.e("decoding invitation bitmap");
+        }
+        return bitmap;
+    }
 
-	public void showInvitation() {
-		mShowInvitation = true;
-		invalidate();
-	}
+    public void showInvitation() {
+        mShowInvitation = true;
+        invalidate();
+    }
 
-	public void hideInvitation() {
-		mShowInvitation = false;
-		invalidate();
-	}
+    public void hideInvitation() {
+        mShowInvitation = false;
+        invalidate();
+    }
 
-	@Override
-	protected void onDraw(@NonNull Canvas canvas) {
-		super.onDraw(canvas);
-		if (mShowInvitation) {
-			canvas.drawBitmap(mBitmap, mLeft, mTop, mPaint);
-		}
-	}
+    @Override
+    protected void onDraw(@NonNull Canvas canvas) {
+        super.onDraw(canvas);
+        if (mShowInvitation) {
+            if (mInvitation != null) {
+                canvas.drawBitmap(mInvitation, mInvitationLeft, mInvitationTop, mPaint);
+            }
+        }
+    }
 
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged(w, h, oldw, oldh);
-		mLeft = w - mBitmap.getWidth() - getPaddingRight();
-		mTop = (h - mBitmap.getHeight()) / 2;
-	}
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        if (mInvitation != null) {
+            mInvitationLeft = w - mInvitation.getWidth() - getPaddingRight();
+            mInvitationTop = (h - mInvitation.getHeight()) / 2;
+        }
+    }
 }
