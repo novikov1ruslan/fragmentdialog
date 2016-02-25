11ba4e3a3c99284c75874dfa11ee315e033e96bb
Novikov, Ruslan <ruslan.novikov@here.com>
2016-02-25 20:25:24 +0100

    adcolony removed, first espresso test added

---
+++ MorskoiBoi/src/androidTest/java/com/ivygames/morskoiboi/FirstTest.java
@@ -0,0 +1,36 @@
+package com.ivygames.morskoiboi;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+
+import com.ivygames.morskoiboi.screen.selectgame.SelectGameLayout;
+
+import org.hamcrest.Matchers;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class FirstTest {
+
+    @Rule
+    public ActivityTestRule<BattleshipActivity> mActivityRule = new ActivityTestRule<>(
+            BattleshipActivity.class);
+
+    @Test
+    public void changeText_sameActivity() {
+        onView(withId(R.id.play)).perform(click());
+        onView(Matchers.<View>instanceOf(SelectGameLayout.class)).check(matches(isDisplayed()));
+//        onView(Matchers.<View>instanceOf(BoardSetupLayout.class)).check(matches(isDisplayed()));
+    }
+}
