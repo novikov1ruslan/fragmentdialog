0aa33dd0bf8cec51bb5192b7ac2abb375dd40f7e
Novikov, Ruslan <ruslan.novikov@here.com>
2016-04-11 00:01:55 +0200

    HelpScreenTest added

---
+++ MorskoiBoi/src/androidTest/java/com/ivygames/morskoiboi/HelpScreenTest.java
@@ -0,0 +1,38 @@
+package com.ivygames.morskoiboi;
+
+import android.view.View;
+
+import com.ivygames.morskoiboi.screen.BattleshipScreen;
+import com.ivygames.morskoiboi.screen.help.HelpScreen;
+import com.ivygames.morskoiboi.screen.main.MainScreenLayout;
+
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+
+
+public class HelpScreenTest extends ScreenTest {
+
+    @Before
+    public void setup() {
+        super.setup();
+    }
+
+    @Override
+    public BattleshipScreen newScreen() {
+        return new HelpScreen(activity());
+    }
+
+    @Test
+    public void when_back_button_pressed__main_screen_opens() {
+        setScreen(newScreen());
+        pressBack();
+        onView(Matchers.<View>instanceOf(MainScreenLayout.class)).check(matches(isDisplayed()));
+    }
+
+}
