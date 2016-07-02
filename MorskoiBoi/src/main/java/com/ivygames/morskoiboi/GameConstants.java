a2f59324db0f65950a67c62d52633c58355ff323
Novikov, Ruslan <ruslan.novikov@here.com>
2016-07-03 01:10:03 +0200

    1.4.15 (79)

--- MorskoiBoi/src/main/java/com/ivygames/morskoiboi/GameConstants.java
+++ MorskoiBoi/src/main/java/com/ivygames/morskoiboi/GameConstants.java
@@ -8,7 +8,7 @@ public final class GameConstants {
 
     public static final String ANALYTICS_KEY = "UA-43473473-1";
 
-//    public static boolean IS_TEST_MODE = false;
-    public static boolean IS_TEST_MODE = true;
+    public static boolean IS_TEST_MODE = false;
+//    public static boolean IS_TEST_MODE = true;
 
 }
