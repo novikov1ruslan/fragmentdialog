948ed53e80b64644a5803603eb9ff530af9337ac
Novikov, Ruslan <ruslan.novikov@here.com>
2016-09-12 19:33:11 +0200

    more achievements tests

--- MorskoiBoi/src/test/java/com/ivygames/morskoiboi/achievement/AchievementsManagerTest.java
+++ MorskoiBoi/src/test/java/com/ivygames/morskoiboi/achievement/AchievementsManagerTest.java
@@ -8,6 +8,8 @@ import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static com.ivygames.morskoiboi.achievement.AchievementsManager.BRAVERY_AND_COURAGE;
+import static com.ivygames.morskoiboi.achievement.AchievementsManager.EXTRA_BRAVERY_AND_COURAGE;
 import static com.ivygames.morskoiboi.achievement.AchievementsManager.MILITARY_ACHIEVEMENTS;
 import static com.ivygames.morskoiboi.achievement.AchievementsManager.NAVAL_MERIT;
 import static com.ivygames.morskoiboi.achievement.AchievementsManager.ORDER_OF_HONOUR;
@@ -72,58 +74,121 @@ public class AchievementsManagerTest {
 
     @Test
     public void IfNavalMeritIsNotUnlocked__ComboUnlocksItAndRevealsOrderOfHonour() {
-        when(settings.isAchievementUnlocked(NAVAL_MERIT)).thenReturn(false);
+        setAchievementUnlocked(NAVAL_MERIT, false);
 
         am.processCombo(1);
 
-        verify(settings, times(1)).unlockAchievement(NAVAL_MERIT);
-        verify(apiClient, times(1)).unlock(NAVAL_MERIT);
-        verify(apiClient, times(1)).reveal(ORDER_OF_HONOUR);
+        verifyUnlocked(NAVAL_MERIT);
+        verifyRevealed(ORDER_OF_HONOUR);
     }
 
     @Test
     public void IfNavalMeritIsNotUnlocked__2xComboUnlocksItAndRevealsOrderOfHonour() {
-        when(settings.isAchievementUnlocked(NAVAL_MERIT)).thenReturn(false);
+        setAchievementUnlocked(NAVAL_MERIT, false);
 
         am.processCombo(2);
 
-        verify(settings, times(1)).unlockAchievement(NAVAL_MERIT);
-        verify(apiClient, times(1)).unlock(NAVAL_MERIT);
-        verify(apiClient, times(1)).reveal(ORDER_OF_HONOUR);
+        verifyUnlocked(NAVAL_MERIT);
+        verifyRevealed(ORDER_OF_HONOUR);
     }
 
     @Test
-    public void IfNavalMeritIsUnlocked__ComboHasNoEffect() {
-        when(settings.isAchievementUnlocked(NAVAL_MERIT)).thenReturn(true);
+    public void IfNavalMeritIsUnlocked__ComboDoNotUnlockOrderOfHonour() {
+        setAchievementUnlocked(NAVAL_MERIT, true);
 
         am.processCombo(1);
 
-        verify(settings, never()).unlockAchievement(NAVAL_MERIT);
-        verify(apiClient, never()).unlock(NAVAL_MERIT);
-        verify(apiClient, never()).reveal(ORDER_OF_HONOUR);
+        verifyNotUnlocked(ORDER_OF_HONOUR);
     }
 
     @Test
     public void IfNavalMeritIsUnlocked__2xComboUnlocksOrderOfHonour() {
-        when(settings.isAchievementUnlocked(NAVAL_MERIT)).thenReturn(false);
+        setAchievementUnlocked(NAVAL_MERIT, true);
 
         am.processCombo(2);
 
-        verify(settings, never()).unlockAchievement(ORDER_OF_HONOUR);
-        verify(apiClient, never()).unlock(ORDER_OF_HONOUR);
+        verifyUnlocked(ORDER_OF_HONOUR);
+    }
+
+//    @Test
+//    public void IfOrderOfHonourIsUnlocked__20xComboHasNoEffect() {
+//        setAchievementUnlocked(ORDER_OF_HONOUR, true);
+//
+//        am.processCombo(20);
+//
+//        verifyNotUnlocked(ORDER_OF_HONOUR);
+//    }
+
+    @Test
+    public void IfBraveryAndCourageIsNotUnlocked__50ShellsUnlocksItAndRevealsExtraBraveryAndCourage() {
+        setAchievementUnlocked(BRAVERY_AND_COURAGE, false);
+
+        am.processShellsLeft(50);
+
+        verifyUnlocked(BRAVERY_AND_COURAGE);
+        verifyRevealed(EXTRA_BRAVERY_AND_COURAGE);
     }
 
     @Test
-    public void IfOrderOfHonourIsUnlocked__20xComboHasNoEffect() {
-        when(settings.isAchievementUnlocked(ORDER_OF_HONOUR)).thenReturn(false);
+    public void IfBraveryAndCourageIsNotUnlocked__60ShellsUnlocksItAndRevealExtraBraveryAndCourage() {
+        setAchievementUnlocked(BRAVERY_AND_COURAGE, false);
 
-        am.processCombo(20);
+        am.processShellsLeft(60);
 
-        verify(settings, never()).unlockAchievement(ORDER_OF_HONOUR);
-        verify(apiClient, never()).unlock(ORDER_OF_HONOUR);
+        verifyUnlocked(BRAVERY_AND_COURAGE);
+        verifyRevealed(EXTRA_BRAVERY_AND_COURAGE);
+    }
+
+    @Test
+    public void IfBraveryAndCourageIsUnlocked__50ShellsDoNotUnlockExtraBraveryAndCourage() {
+        setAchievementUnlocked(BRAVERY_AND_COURAGE, true);
+
+        am.processShellsLeft(50);
+
+        verifyNotUnlocked(EXTRA_BRAVERY_AND_COURAGE);
+    }
+
+    @Test
+    public void IfBraveryAndCourageIsUnlocked__60ShellsUnlockExtraBraveryAndCourage() {
+        setAchievementUnlocked(BRAVERY_AND_COURAGE, true);
+
+        am.processShellsLeft(60);
+
+        verifyUnlocked(EXTRA_BRAVERY_AND_COURAGE);
+    }
+
+//    @Test
+//    public void IfExtraBraveryAndCourageIsUnlocked__100ShellsHaveNoEffect() {
+//        setAchievementUnlocked(EXTRA_BRAVERY_AND_COURAGE, true);
+//
+//        am.processShellsLeft(100);
+//
+//        verifyNotUnlocked(EXTRA_BRAVERY_AND_COURAGE);
+//    }
+
+    private void verifyNotRevealed(String achievementId) {
+        verify(apiClient, never()).reveal(achievementId);
+    }
+
+    private void verifyRevealed(String achievement) {
+        verify(apiClient, times(1)).reveal(achievement);
     }
 
     private void setMilitaryAchievementsUnlocked(boolean value) {
-        when(settings.isAchievementUnlocked(MILITARY_ACHIEVEMENTS)).thenReturn(value);
+        setAchievementUnlocked(MILITARY_ACHIEVEMENTS, value);
+    }
+
+    private void setAchievementUnlocked(String achievement, boolean unlocked) {
+        when(settings.isAchievementUnlocked(achievement)).thenReturn(unlocked);
+    }
+
+    private void verifyUnlocked(String braveryAndCourage) {
+        verify(settings, times(1)).unlockAchievement(braveryAndCourage);
+        verify(apiClient, times(1)).unlock(braveryAndCourage);
+    }
+
+    private void verifyNotUnlocked(String achievement) {
+        verify(settings, never()).unlockAchievement(achievement);
+        verify(apiClient, never()).unlock(achievement);
     }
 }
