35494fb199009e86bddaaa8f42a144402376106e
Novikov, Ruslan <ruslan.novikov@here.com>
2016-11-23 15:25:03 +0100

    Remove equals from Coordinate

--- MorskoiBoi/src/main/java/com/ivygames/battleship/board/Coord.java
+++ MorskoiBoi/src/main/java/com/ivygames/battleship/board/Coord.java
@@ -41,33 +41,6 @@ public final class Coord {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + i;
-        result = prime * result + j;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        Coord other = (Coord) obj;
-        if (i != other.i) {
-            return false;
-        }
-        return j == other.j;
-    }
-
-    @Override
     public String toString() {
         return "[" + i + "," + j + "]";
     }
