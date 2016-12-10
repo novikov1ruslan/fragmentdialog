fd26eedc0399cde1febef622ccc675abf8198d8f
Novikov, Ruslan <ruslan.novikov@here.com>
2016-12-10 01:45:05 +0100

    Remove RESERVED Cell

--- MorskoiBoi/src/main/java/com/ivygames/battleship/board/Cell.java
+++ MorskoiBoi/src/main/java/com/ivygames/battleship/board/Cell.java
@@ -2,8 +2,6 @@ package com.ivygames.battleship.board;
 
 public enum Cell {
     EMPTY,
-    // TODO: this is not really needed
-    RESERVED,
     MISS,
     HIT;
 
--- MorskoiBoi/src/main/java/com/ivygames/battleship/board/CellSerialization.java
+++ MorskoiBoi/src/main/java/com/ivygames/battleship/board/CellSerialization.java
@@ -4,7 +4,6 @@ import android.support.annotation.NonNull;
 
 public class CellSerialization {
     private final static char EMPTY_VAL = ' ';
-    private final static char RESERVED_VAL = '0';
     private final static char MISS_VAL = '*';
     private final static char HIT_VAL = 'X';
 
@@ -17,40 +16,25 @@ public class CellSerialization {
     @NonNull
     public static Cell parse(char c) {
         switch (c) {
-            case RESERVED_VAL:
-                return Cell.RESERVED;
             case MISS_VAL:
                 return Cell.MISS;
             case HIT_VAL:
                 return Cell.HIT;
             case EMPTY_VAL:
-                return Cell.EMPTY;
             default:
-                return parseProximityCell(c);
-        }
-    }
-
-    @NonNull
-    private static Cell parseProximityCell(char c) {
-        int zero = '0';
-        if (c >= zero && c <= zero + LEGACY_RESERVED_PROXIMITY_VALUE) {
-            return Cell.RESERVED;
-        } else {
-            throw new IllegalArgumentException(Character.toString(c));
+                return Cell.EMPTY;
         }
     }
 
     public static char toChar(@NonNull Cell cell) {
         switch (cell) {
-            case EMPTY:
-                return EMPTY_VAL;
             case MISS:
                 return MISS_VAL;
             case HIT:
                 return HIT_VAL;
+            case EMPTY:
             default:
-            case RESERVED:
-                return RESERVED_VAL;
+                return EMPTY_VAL;
         }
     }
 }
